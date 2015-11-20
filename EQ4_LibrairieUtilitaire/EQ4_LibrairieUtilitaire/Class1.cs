20ff45a20b6c64755a911542dc50bed5e1f302d3
DaFt2794 <lucas.laurent27@gmail.com>
2015-11-20 11:24:08 +0100

    Ajout de la bibliothèque EQ4_Librairie
    Ajout classe

---
+++ EQ4_LibrairieUtilitaire/EQ4_LibrairieUtilitaire/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQ4_LibrairieUtilitaire
+{
+    public class Class1
+    {
+    }
+}
