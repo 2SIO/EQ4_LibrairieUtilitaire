7148696d98c5bc75dff0748399fd3dfb59e3b995
Biavardi <biavardi.b@gmail.com>
2015-11-20 11:49:54 +0100

    test
    test

--- EQ4_LibrairieUtilitaire/EQ4_LibrairieUtilitaire/Class1.cs
+++ EQ4_LibrairieUtilitaire/EQ4_LibrairieUtilitaire/Class1.cs
@@ -8,5 +8,29 @@ namespace EQ4_LibrairieUtilitaire
 {
     public class Class1
     {
+        string message = " ";
+        #region ouverture de connexion
+        public static String OpenConnexion(String SonLogin,String SonMdp,String SonServeur,String SaBD, out Boolean verif, out String )
+        {
+            String Message = " ";
+
+            SqlConnection gObjetConnection = new SqlConnection();
+
+            gObjetConnection.ConnectionString = "User ID =" + SonLogin + ";PWD= " + SonMdp + "; Server= " + SonServeur + "; Database=" + SaBD+ ";";
+
+            try
+            {
+                
+                gObjetConnection.Open();
+                Message = "Connexion Reussie";
+            }
+            catch (System.Data.SqlClient.SqlException probleme)
+            {
+                
+               Message = "Connexion échoué"+probleme;
+            }
+            return Message;
+        }
+        #endregion
     }
 }
